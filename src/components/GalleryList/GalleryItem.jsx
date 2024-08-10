3eff9941551641beaf579c6c55710b2b30d88cfc
Emma Molden <emma.molden007@gmail.com>
2024-08-10 11:59:02 -0500

    toggle working

--- src/components/GalleryList/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem.jsx
@@ -1,9 +1,13 @@
 
 import './GalleryList.css';
 import axios from 'axios';
+import { useState } from 'react';
 
 
 function GalleryItem ({img, fetchImages}) {
+
+    const [imageDescription, setImageDescription] = useState('Image');
+    const [imageDescriptionDisplay, setImageDescriptionDisplay] = useState(<img className="image" src={img.url}/>);
 
     const addLike = () => {
         console.log('in addLike function');
@@ -19,13 +23,25 @@ function GalleryItem ({img, fetchImages}) {
         })
     }
 
+    const changeImageDescription = () => {
+        if(imageDescription === 'Image') {
+            setImageDescription('Description');
+            setImageDescriptionDisplay(<p>{img.description}</p>)
+        }
+        else if (imageDescription === 'Description') {
+            setImageDescription('Image');
+            setImageDescriptionDisplay(<img className="image" src={img.url}/>)
+        }
+    }
+
     return (
             <li className= "singleItem" data-testid="galleryItem">
                 <h3>{img.title}</h3>
-                <img className="image" src={img.url}/>
-                <p>{img.description}</p>
+                <div>
+                    {imageDescriptionDisplay}
+                </div>
                 <span className="buttons">
-                    <button data-testid="toggle">description/image</button>
+                    <button data-testid="toggle" onClick={changeImageDescription}>description/image</button>
                     <button data-testid="like" onClick={addLike}>{img.likes} Likes</button>
                 </span>
             </li>
